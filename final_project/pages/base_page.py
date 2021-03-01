9817d7049fb324cd4c75cf78f4807548192253d0
user <pinkwhale.lr@yandex.ru>
2021-03-01 21:38:45 +0300

    ready for review

--- final_project/pages/base_page.py
+++ final_project/pages/base_page.py
@@ -1,26 +1,25 @@
 
 from selenium.common.exceptions import NoSuchElementException
 from selenium.common.exceptions import NoAlertPresentException
-import math
 from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from .locators import BasePageLocators
+import math
 
 class BasePage():
-    def __init__(self, browser, url, timeout=30):
+    def __init__(self, browser, url, timeout=10):
         self.browser = browser
         self.url = url
         self.browser.implicitly_wait(timeout)
 
-    def open(self):
-        self.browser.get(self.url)
+    def go_to_basket_page(self):
+        link = self.browser.find_element(*BasePageLocators.BASKET_LINK)
+        link.click()
 
-    def is_element_present(self, how, what):
-        try:
-            self.browser.find_element(how, what)
-        except (NoSuchElementException):
-            return False
-        return True
+    def go_to_login_page(self):
+        link = self.browser.find_element(*BasePageLocators.LOGIN_LINK)
+        link.click()
 
     def is_disappeared(self, how, what, timeout=4):
         try:
@@ -30,9 +29,12 @@ class BasePage():
             return False
         return True
 
-#    def is_not_element_present(self):
-#        assert self.is_element_present(*ProductPageLocators.SUCCESS_MESSAGE), \
-#            "Success message is presented, but should not be"
+    def is_element_present(self, how, what):
+        try:
+            self.browser.find_element(how, what)
+        except (NoSuchElementException):
+            return False
+        return True
 
     def is_not_element_present(self, how, what, timeout=4):
         try:
@@ -41,11 +43,20 @@ class BasePage():
             return True
         return False
 
+    def open(self):
+        self.browser.get(self.url)
+
+    def should_be_authorized_user(self):
+        assert self.is_element_present(*BasePageLocators.USER_ICON), "User icon is not presented," \
+                                                                     " probably unauthorised user"
+
+    def should_be_login_link(self):
+        assert self.is_element_present(*BasePageLocators.LOGIN_LINK), "Login link is not presented"
+
     def solve_quiz_and_get_code(self):
         alert = self.browser.switch_to.alert
         x = alert.text.split(" ")[2]
         answer = str(math.log(abs((12 * math.sin(float(x))))))
-        #print(x, answer, type(x), type(answer))
         alert.send_keys(answer)
         alert.accept()
         try:
@@ -55,3 +66,4 @@ class BasePage():
             alert.accept()
         except NoAlertPresentException:
             print("No second alert presented")
+
