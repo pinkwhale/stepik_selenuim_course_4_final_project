717ca9b2830873aea9933ee2e2bf24c732f4dffa
user <pinkwhale.lr@yandex.ru>
2021-03-01 21:49:39 +0300

    ready for review

--- final_project/pages/product_page.py
+++ final_project/pages/product_page.py
@@ -8,15 +8,9 @@ class ProductPage(BasePage):
         button = self.browser.find_element(*ProductPageLocators.ADD_TO_BASKET_BUTTON)
         button.click()
 
-    def go_to_basket_page(self):
-        link = self.browser.find_element(*ProductPageLocators.BASKET_LINK)
-        link.click()
-
     def should_match_product_name(self):
         product_name = self.browser.find_element(*ProductPageLocators.PRODUCT_NAME).text
-        #print(product_name)
         added_product_name = self.browser.find_element(*ProductPageLocators.ADDED_PRODUCT_NAME).text
-        #print(added_product_name)
         assert product_name == added_product_name, f"Product name does not match, should {product_name}, given {added_product_name}"
 
     def should_match_product_price(self):
